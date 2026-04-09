be098001ecdfbc7a8ff31f3f89c9d2995dae5d56
LFPINOSC <lfpinosc@ucacue.edu.ec>
2026-04-09 16:23:58 -0500

    g

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import ClienteScreen from './src/Screen/ClienteScreen';
+import HomeScreen from './src/Screen/HomeScreen';
 import { ClienteServicio } from './src/Services/ClienteServicio';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer} from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <ClienteServicio>
       <NavigationContainer>
-        <StackActions.Navigator>
-          <StackActions.Screen name="Home" component={HomeScreen} />
-          <StackActions.Screen name="ClienteScreen" component={ClienteScreen} />
-          <StackActions.Screen name="ClienteForm" component={ClienteForm} />
-        </StackActions.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ClienteScreen" component={ClienteScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </ClienteServicio>
   );
